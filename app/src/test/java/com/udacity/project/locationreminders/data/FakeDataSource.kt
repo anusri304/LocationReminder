5aa87a3a5145661401f4f68e0429bf2fd80e8cbe
ksdc-anandhid <Anandhi.Dharmarajan@ksdc.co.za>
2022-11-04 10:05:14 +0200

    Added tests for viewmodel

--- app/src/test/java/com/udacity/project/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project/locationreminders/data/FakeDataSource.kt
@@ -1,26 +1,54 @@
 package com.udacity.project.locationreminders.data
 
 import com.udacity.project.locationreminders.data.dto.ReminderDTO
+import com.udacity.project.locationreminders.data.dto.Result
+
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    var reminderDTOList = mutableListOf<ReminderDTO>()
+
+    private var shouldReturnError = false
+
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
+
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+         try {
+            if (shouldReturnError) {
+                return Result.Error(("No Reminders Found"))
+            }
+            return Result.Success(ArrayList(reminderDTOList))
+        }
+        catch (ex:Exception) {
+            return Result.Error(ex.message)
+        }
+
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminderDTOList.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        try {
+            val reminder = reminderDTOList.find { it.id == id }
+            if ( reminder ==null  || shouldReturnError) {
+                return Result.Error(("No Reminder Found with id $id"))
+            } else {
+               return  Result.Success(reminder)
+            }
+        }
+        catch (ex:Exception) {
+            return Result.Error(ex.message)
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+      reminderDTOList.clear()
     }
 
 
